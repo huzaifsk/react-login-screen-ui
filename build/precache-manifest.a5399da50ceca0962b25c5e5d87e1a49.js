db7d4a827c81cf2398126c3a0964841a66059392
huzaifsk <huzaifsk12@gmail.com>
2022-12-22 16:12:44 +0530

    added build files

---
+++ build/precache-manifest.a5399da50ceca0962b25c5e5d87e1a49.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ecd4edc4ebcfc2889a3f6e21df3cae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c48b2defec933a868b6",
+    "url": "/static/js/2.5a6daf66.chunk.js"
+  },
+  {
+    "revision": "1a936c5e2d10a4ae7e75",
+    "url": "/static/js/main.74a705dc.chunk.js"
+  },
+  {
+    "revision": "5fedce0193e352065b9c",
+    "url": "/static/js/runtime-main.7b864e03.js"
+  }
+]);
